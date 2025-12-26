8116e2378aa28068400597a7b4487340138b43da
OmKadam-1 <kom110798@gmail.com>
2025-12-27 02:11:32 +0530

    url change

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,18 @@ function App() {
 
     const todo = async () => {
         console.log("Loading");
-        const response = await axios.get("http://localhost:8030/todos");
+        const response = await axios.get("https://todoserver-pojm.onrender.com/");
 
 
         setTodos(response.data.data);
     };
     const add = async () => {
-        const response = await axios.post("http://localhost:8030/todos", { todoitem: newtodo },);
+        const response = await axios.post("https://todoserver-pojm.onrender.com/", { todoitem: newtodo },);
         setnewtodo("");
         todo();
     }
     const edit = async() =>{
-        const response = await axios.put("http://localhost:8030/todos",{
+        const response = await axios.put("https://todoserver-pojm.onrender.com/",{
             "oldtodoitem":oldtodo,
             "newtodoitem":newtodo,
         });
@@ -38,7 +38,7 @@ function App() {
         setoldtodo("");
     }
     const undo = async (todoitem) => {
-        const response = await axios.delete("http://localhost:8030/todos", { data: { todoitem: todoitem } },);
+        const response = await axios.delete("https://todoserver-pojm.onrender.com/", { data: { todoitem: todoitem } },);
         todo();
     }
 
