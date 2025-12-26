13f0e7f7b8cd99961faddfc22ed53cb33038da81
OmKadam-1 <kom110798@gmail.com>
2025-12-27 03:29:42 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Trash, SquarePen } from 'lucide-react';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios, { Axios } from "axios";
+import imgdelete from "./assets/delete.png";
+import editimg from "./assets/edit.png";
+
+
+
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setnewTodo] = useState("");
-  const [oldTodo, setOldTodo] = useState("");
-  const [editMode, setEditMode] = useState(false);
-const baseUrl=import.meta.env.VITE_BASE_URL;
 
-  const loadTodos = async () => {
-    console.log("Loading todos...");
-    const response = await axios.get(`${baseUrl}/todos`);
+    const [todos, setTodos] = useState([]);
+    const [oldtodo, setoldtodo] = useState("");
+    const [editmode, seteditmode] = useState("false");
+    const [newtodo, setnewtodo] = useState("");
 
-    setTodos(response.data.data);
-  }
-  const addTodo = async () => {
-    const response = await axios.post(`${baseUrl}/todos`, { "todoItems": newTodo });
-    setnewTodo("");
-    loadTodos();
-
-  }
-  const deleteTodo = async (todoItem) => {
-    const response = await axios.delete(`${baseUrl}/todos`, {
-      data: { todoItem },
-    });
-    loadTodos();
-  }
-
-  const editTodo = async () => {
-    const response = await axios.put(`${baseUrl}/todos`, {
-      "newTodoItem": newTodo,
-      "oldTodoItem": oldTodo
-
-    })
-    loadTodos();
-    setEditMode(false);
-    setnewTodo("");
-    setOldTodo("");
-  }
-  useEffect(() => {
-    loadTodos();
-  }, []);
-  return (
-    <div className='bg-[#EAE0CF] min-h-[100vh] ' >
-      <h1 className='text-4xl text-[#213448] font-bold text-center p-[20px]'>ToDo list</h1>
-
-      <div className='flex flex-col justify-center items-center w-full h-[400px] scroll-auto overflow-y-scroll '>
-        {
-          todos.map((todo, index) => {
-            return (
-              <div key={index} className='w-[90%] md:w-[70%] border-1 px-[20px] py-[15px] m-[10px] rounded-[10px] flex justify-between items-center text-[18px] bg-[#94B4C1]'>
-                <p>{todo}</p>
-                <div className='flex gap-2 items-center '
-                 >
-                  <SquarePen className='h-[25px] cursor-pointer'  onClick={() => {
-                    setEditMode(true);
-                    setOldTodo(todo);
-                    setnewTodo(todo);
-                  }
-
-                  } />
-                  <Trash onClick={() => deleteTodo(todo)} className='cursor-pointer h-[25px]  text-red-700 ' /></div>
-              </div>
-            )
-          })
-        }
-
-      </div>
-      <div className='w-[100%] bg-[#EAE0CF] fixed bottom-0 right-0 left-0 flex justify-center items-center gap-2'>
-        <input type="text" placeholder='Add task' value={newTodo} onChange={(e) => {
-          setnewTodo(e.target.value);
-        }}
-          className='m-[20px] p-[10px] outline-none rounded-lg border-1 w-[50%] md:w-[50%] border-[#213448]' />
-        <button className='border text-[14px] md:text-[16px] rounded-[8px] p-[10px] text-[#213448] font-bold cursor-pointer' onClick={() => {
-          if (editMode) {
-            editTodo();
-          }
-          else {
-            addTodo();
-          }
-        }}>{editMode ? "Update TODO" : "Add TODO"}</button>
-      </div>
+    const BASE_URL= import.meta.env.VITE_BASE_URL;
 
 
-    </div>
 
-  )
+    const todo = async () => {
+        console.log("Loading");
+        const response = await axios.get(`${BASE_URL}`);
+
+
+        setTodos(response.data.data);
+    };
+    const add = async () => {
+        const response = await axios.post(`${BASE_URL}, { todoitem: newtodo },`);
+        setnewtodo("");
+        todo();
+    }
+    const edit = async() =>{
+        const response = await axios.put(`${BASE_URL},{
+            "oldtodoitem":oldtodo,
+            "newtodoitem":newtodo,
+        }`);
+        todo();
+        seteditmode(false);
+        setnewtodo("");
+        setoldtodo("");
+    }
+    const undo = async (todoitem) => {
+        const response = await axios.delete(`${BASE_URL}, { data: { todoitem: todoitem } },`);
+        todo();
+    }
+
+    useEffect(() => {
+        todo();
+    }, []);
+
+    return (
+        <div>
+            <h1 className="title">To do list</h1>
+            {todos.map((todo, index) => {
+                return (
+                    <div key={index} className="todo-container">
+                        <h2>{todo}</h2>
+                        <img src={editimg} className="edit-img" onClick={() => {
+                            seteditmode(true); setoldtodo(todo); setnewtodo(todo);
+                        }}></img>
+                        <img src={imgdelete} className="delete-icon" onClick={() => {
+                            undo(todo);
+                        }}></img>
+                    </div>
+                );
+            })}
+            <div className="container">
+                <input type="text" placeholder="add task" className="input-task" value={newtodo} onChange={(e) => {
+                    setnewtodo(e.target.value);
+                }}></input>
+                <button className="btn-add" onClick={() => {
+                    if (editmode) {
+                        edit();
+                    } else {
+                        add();
+                    }
+                }}>
+                    {editmode ? "Update Todo" : "Add Todo"}
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App
