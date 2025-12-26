9f839060fe785031c0c238727dcd146d342e19a2
OmKadam-1 <kom110798@gmail.com>
2025-12-27 03:10:38 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,85 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
+import axios, { Axios } from "axios";
 import imgdelete from "./assets/delete.png";
 import editimg from "./assets/edit.png";
 
+
+
+
 function App() {
 
-  const [todos, setTodos] = useState([]);
-  const [oldtodo, setoldtodo] = useState("");
-  const [editmode, seteditmode] = useState(false);
-  const [newtodo, setnewtodo] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [oldtodo, setoldtodo] = useState("");
+    const [editmode, seteditmode] = useState("false");
+    const [newtodo, setnewtodo] = useState("");
 
-  const BASE_URL = "https://todoserver-pojm.onrender.com";
 
-  const todo = async () => {
-    const response = await axios.get(BASE_URL);
-    console.log(response.data);
-    setTodos(response.data);
-  };
 
-  const add = async () => {
-    if (!newtodo.trim()) return;
-    await axios.post(BASE_URL, { todoitem: newtodo });
-    setnewtodo("");
-    todo();
-  };
 
-  const edit = async () => {
-    await axios.put(BASE_URL, {
-      oldtodoitem: oldtodo,
-      newtodoitem: newtodo,
-    });
-    seteditmode(false);
-    setnewtodo("");
-    setoldtodo("");
-    todo();
-  };
+    const todo = async () => {
+        console.log("Loading");
+        const response = await axios.get("https://todoserver-pojm.onrender.com");
 
-  const undo = async (todoitem) => {
-    await axios.delete(BASE_URL, {
-      data: { todoitem }
-    });
-    todo();
-  };
 
-  useEffect(() => {
-    todo();
-  }, []);
+        setTodos(response.data.data);
+    };
+    const add = async () => {
+        const response = await axios.post("https://todoserver-pojm.onrender.com, { todoitem: newtodo },");
+        setnewtodo("");
+        todo();
+    }
+    const edit = async () => {
+        const response = await axios.put(`https://todoserver-pojm.onrender.com,{
+            "oldtodoitem":oldtodo,
+            "newtodoitem":newtodo,
+        }`);
+        todo();
+        seteditmode(false);
+        setnewtodo("");
+        setoldtodo("");
+    }
+    const undo = async (todoitem) => {
+        const response = await axios.delete("https://todoserver-pojm.onrender.com, { data: { todoitem: todoitem } },");
+        todo();
+    }
 
-  return (
-    <div>
-      <h1 className="title">To do list</h1>
+    useEffect(() => {
+        todo();
+    }, []);
 
-      {todos.map((todo, index) => (
-        <div key={index} className="todo-container">
-          <h2>{todo}</h2>
-          <img
-            src={editimg}
-            className="edit-img"
-            onClick={() => {
-              seteditmode(true);
-              setoldtodo(todo);
-              setnewtodo(todo);
-            }}
-          />
-          <img
-            src={imgdelete}
-            className="delete-icon"
-            onClick={() => undo(todo)}
-          />
+    return (
+        <div>
+            <h1 className="title">To do list</h1>
+            {todos.map((todo, index) => {
+                return (
+                    <div key={index} className="todo-container">
+                        <h2>{todo}</h2>
+                        <img src={editimg} className="edit-img" onClick={() => {
+                            seteditmode(true); setoldtodo(todo); setnewtodo(todo);
+                        }}></img>
+                        <img src={imgdelete} className="delete-icon" onClick={() => {
+                            undo(todo);
+                        }}></img>
+                    </div>
+                );
+            })}
+            <div className="container">
+                <input type="text" placeholder="add task" className="input-task" value={newtodo} onChange={(e) => {
+                    setnewtodo(e.target.value);
+                }}></input>
+                <button className="btn-add" onClick={() => {
+                    if (editmode) {
+                        edit();
+                    } else {
+                        add();
+                    }
+                }}>
+                    {editmode ? "Update Todo" : "Add Todo"}
+                </button>
+            </div>
         </div>
-      ))}
-
-      <div className="container">
-        <input
-          type="text"
-          placeholder="add task"
-          className="input-task"
-          value={newtodo}
-          onChange={(e) => setnewtodo(e.target.value)}
-        />
-
-        <button
-          className="btn-add"
-          onClick={editmode ? edit : add}
-        >
-          {editmode ? "Update Todo" : "Add Todo"}
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
 
-export default App;
+export default App
